b1ef5451e848a4815471e0971550c20690dc2250
Aliza02 <alizaanxari08@gmail.com>
2022-06-14 13:11:05 +0500

    day8 js

--- week1/day8-soundboard.js
+++ week1/day8-soundboard.js
@@ -4,5 +4,18 @@ sounds.forEach(sound=>{
     btn.classList.add('btn');
     btn.innerHTML=sound;
 
+    btn.addEventListener('click',()=>{
+        stopsongs();
+        document.getElementById(sound).play();
+    })
+
     document.getElementById('buttons').appendChild(btn);
 })
+function stopsongs()
+{
+    sounds.forEach(sound=>{
+        const song=document.getElementById(sound);
+        song.pause();
+        song.currentTime=0;
+    })
+}
